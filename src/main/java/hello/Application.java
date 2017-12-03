d2fc2ff64a5a93b6f7c80cae0d7d32ee59cce753
kammana <kammana@192.168.50.1>
2017-12-03 15:26:41 +0530

    Added changes

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World";
+        return "<h1>Hello Docker World</h1>";
     }
 
     public static void main(String[] args) {
