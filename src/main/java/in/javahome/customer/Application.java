0ebc58c939ab4c2da60f3abc4607e11001dcdc59
kammana <kammana@192.168.1.6>
2017-12-29 11:37:44 +0530

    Added UI related stuff

--- src/main/java/in/javahome/customer/Application.java
+++ src/main/java/in/javahome/customer/Application.java
@@ -2,35 +2,36 @@ package in.javahome.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import in.javahome.customer.service.CustomerService;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class Application {
-	
 
-    @RequestMapping("/")
-    public String home() {
-        return "<h1>Welcome to customer App</h1>";
-    }
-    @RequestMapping(path="customer/{id}",method=RequestMethod.GET)
-    public String customer(@PathVariable("id") int custId) {
-    		StringBuffer data = new StringBuffer();
-    		data.append("<h3> Customer </h3>")
-    		.append("Customer Id "+CustomerService.getCustomer(custId))
-    		.append("\n <h3> Orders </h3>")
-    		.append(OrdersClientAPI.callOrderApi(custId));
-        return data.toString();
-    }
-    
+	@RequestMapping("/")
+	public String home() {
+		return "welcome.jsp";
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	@RequestMapping(path = "customer/{id}", method = RequestMethod.GET)
+	public ModelAndView customer(@PathVariable("id") int custId, ModelMap map) {
+		String hostName = System.getenv("HOSTNAME") == null ? "localhost" : System.getenv("HOSTNAME");
+		map.put("hostName", hostName);
+		map.put("customerName", CustomerService.getCustomer(custId));
+		map.put("orderDetails", OrdersClientAPI.callOrderApi(custId));
+		ModelAndView mv = new ModelAndView("welcome.jsp",map);
+		return mv;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
 }
